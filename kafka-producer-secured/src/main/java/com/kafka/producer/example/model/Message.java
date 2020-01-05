13d448bda7485d085a482a5557346152cba0d0a9
sendkumaranil <anil.verma4283@gmail.com>
2020-01-05 22:27:20 +0530

    added secured kafka producer

---
+++ kafka-producer-secured/src/main/java/com/kafka/producer/example/model/Message.java
@@ -0,0 +1,22 @@
+package com.kafka.producer.example.model;
+
+public class Message {
+    private String messageId;
+    private String messages;
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getMessages() {
+        return messages;
+    }
+
+    public void setMessages(String messages) {
+        this.messages = messages;
+    }
+}
---
+++ kafka-producer-secured/src/main/java/com/kafka/producer/example/producer/KafkaProducer.java
@@ -0,0 +1,48 @@
+package com.kafka.producer.example.producer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka.producer.example.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/kafka-producer")
+public class KafkaProducer {
+
+    //@Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+
+    @Value("${kafka.producer.topics}")
+    private String topics;
+
+    public KafkaProducer(KafkaTemplate<String,String> kafkaTemplate){
+        this.kafkaTemplate=kafkaTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody Message message){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String msg = mapper.writeValueAsString(message);
+            kafkaTemplate.send(topics,msg);
+        }catch(Exception e){
+            //handle exception here
+            return "Error Occurred:"+e.getMessage();
+        }
+        return "Message Sent To Kafka Topics: "+topics;
+    }
+
+    @GetMapping("/send")
+    public String send(@RequestParam("message") String message){
+        try {
+            kafkaTemplate.send(topics,message);
+        }catch(Exception e){
+            //handle exception here
+            return "Error Occurred:"+e.getMessage();
+        }
+        return "Message Sent To Kafka Topics: "+topics;
+    }
+
+}
